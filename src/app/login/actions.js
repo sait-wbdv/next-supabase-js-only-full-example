aa3c2c71857c2c32ed5e82ac1f57297dae086b54
Ashlyn Knox <ashlyn@lilyx.io>
2024-12-04 15:15:25 -0700

    login action error handling

--- src/app/login/actions.js
+++ src/app/login/actions.js
@@ -4,3 +4,11 @@ import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 import { createClient } from "@/utils/supabase/server";
+
+export async function login(formData) {
+  const client = await createClient();
+  const { error } = await supabase.auth.signInWithPassword(data);
+  if (error) {
+    redirect("/error");
+  }
+}
