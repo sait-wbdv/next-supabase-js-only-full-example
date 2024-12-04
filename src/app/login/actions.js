daef31d6b4bd9b9d5677ea8a6bb72d1fd794feae
Ashlyn Knox <ashlyn@lilyx.io>
2024-12-04 15:17:22 -0700

    login action form data

--- src/app/login/actions.js
+++ src/app/login/actions.js
@@ -7,6 +7,10 @@ import { createClient } from "@/utils/supabase/server";
 
 export async function login(formData) {
   const supabase = await createClient();
+  const data = {
+    email: formData.get("email"),
+    password: formData.get("password"),
+  };
   const { error } = await supabase.auth.signInWithPassword(data);
   if (error) {
     redirect("/error");
