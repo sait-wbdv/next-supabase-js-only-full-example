5f71aaa24f7db2f1631e50d144b79c6600d793ee
Ashlyn Knox <ashlyn@lilyx.io>
2024-12-04 17:50:14 -0700

    small examples on accessing information

--- src/app/components/logout.jsx
+++ src/app/components/logout.jsx
@@ -4,8 +4,16 @@ import { createClient } from "@/utils/supabase/client";
 const supabase = createClient();
 
 async function logout() {
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (user) {
+    alert(user.email);
+  }
   const { error } = await supabase.auth.signOut();
-  alert("logged out. verify by checking cookies in localStorage");
+  if (!user) {
+    alert("logged out. verify by checking cookies in localStorage");
+  }
   if (error) {
     console.error("Logout Error", error);
   }
